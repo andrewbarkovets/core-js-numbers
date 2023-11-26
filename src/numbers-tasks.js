cfe0236ccdbc34a088e97b04036e4d1857b894a6
andrewBar-ko <andrewbarkovets>
2023-11-26 15:25:10 +0300

    refactor: change structure getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -298,8 +298,8 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return num % 9;
 }
 
 /**
